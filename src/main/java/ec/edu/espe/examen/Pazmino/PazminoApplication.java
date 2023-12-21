eadd9933e188a684fe57db4d8655debca3f3fb24
bpbryalex98 <bpbryalex98@gmail.com>
2023-12-20 19:24:33 -0500

    Creacion Examen Repositorio

---
+++ src/main/java/ec/edu/espe/examen/Pazmino/PazminoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen.Pazmino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PazminoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PazminoApplication.class, args);
+	}
+
+}
